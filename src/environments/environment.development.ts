f2c0bfa9aa296ff76a090e0bba0474664662e640
pmv612 <piezo115@outlook.com>
2024-09-05 19:19:56 -0500

    chore: configured environment variables.

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,8 @@
+// Environment: development
+// This is the development environment configuration file.
+// It contains the configuration settings for the development environment.
+// Note: In real scenarios, this file is not committed to the source control.
+export const environment = {
+  production: false,
+  newsApiKey: '77d53f3405e94bca81ab4ac91365745c'
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,8 @@
+// Environment: production
+// This is the production environment configuration file.
+// It contains the configuration settings for the production environment.
+// Note: In real scenarios, this file is not committed to the source control.
+export const environment = {
+  production: true,
+  newsApiKey: '77d53f3405e94bca81ab4ac91365745c'
+};
